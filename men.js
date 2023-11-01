5b5764cb1839ed21518bad42e95b9c023b8b6dd6
leticiayukariukuma <137304838+leticiayukariukuma@users.noreply.github.com>
2023-11-01 10:48:34 -0300

    .

--- men.js
+++ men.js
@@ -2,7 +2,11 @@ function tocaSomPom(){
 document.querySelector('#som_tecla_pom').play ('');
 }
 
+document.querySelectoA11('.tecla')
 
-
-
+const listadeteclado[0].onclik = tocaSomPom
 document.querySelector('.tecla_pom').onclik= tocaSomPom();
+while(){
+
+}
+
