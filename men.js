03b7958f588e5f738a0d42b9df5889438a566e54
leticiayukariukuma <137304838+leticiayukariukuma@users.noreply.github.com>
2023-11-01 10:56:32 -0300

    .

--- men.js
+++ men.js
@@ -1,12 +1,14 @@
-function tocaSomPom(){
-document.querySelector('#som_tecla_pom').play ('');
+function tocaSomPom(iElementoAudio){
+document.querySelector('#som_tecla_pom').play (iElementoAudio);
 }
 
 document.querySelectoA11('.tecla')
 
 const listadeteclado[0].onclik = tocaSomPom
 document.querySelector('.tecla_pom').onclik= tocaSomPom();
-while(){
-
-}
+while(contador < 9){
+listadeteclas[contador].onclik=fuction(){
+tocaSom('#som_tecla_clap');
+};
+const tecla= listaDeTeclas[contador];
 
